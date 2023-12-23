2ec42f87045d65d48466a92d46749ac1ee3375ec
AcId4848 <142092198+AcId4848@users.noreply.github.com>
2023-12-23 15:03:53 +0700

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,12 @@
 public class Character {
-
+  public int fear_level;
+  public int hp;
+  public int bonus;
+  public String name;
+  public Character(String name) {
+      fear_level = 50;
+      hp = 100;
+      bonus = 0;
+    this.name = name;
+  }
 }
