d846ddc49e45e53b07c373e405137180b24a05da
AcId4848 <142092198+AcId4848@users.noreply.github.com>
2023-12-23 15:11:47 +0700

    Update Situation.java

--- src/Situation.java
+++ src/Situation.java
@@ -1,3 +1,13 @@
 public class Situation {
-
+  Situation[] direction;
+  String subject, text;
+  int dFear_level, dHP, dBonus;
+  public Situation (String subject, String text, int variants, int dfear_level, int dhp, int dbonus) {
+    this.subject = subject;
+    this.text = text;
+    dFear_level = dfear_level;
+    dHP = dhp;
+    dBonus= dbonus;
+    direction = new Situation[variants];
+  }
 }
